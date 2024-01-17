9757486c8bba06fb22f2d6aef34d6af626597f65
0h My Goat <a.devos@octocorn.fr>
2024-01-17 11:55:12 +0100

    chore: init project :tada:

---
+++ src/main/java/fr/octocorn/happyningbackend/HappyningBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.octocorn.happyningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyningBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/octocorn/happyningbackend/HappyningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.octocorn.happyningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
